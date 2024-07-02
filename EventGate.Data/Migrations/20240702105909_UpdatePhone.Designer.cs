686887312e7c9f8170e5fd1c551841914e3bc9a6
psp-xuanquy <you@example.comxquy118@gmail.com>
2024-07-02 18:00:58 +0700

    update Phone in UserHistory

--- EventGate.Data/Migrations/20240702105909_UpdatePhone.Designer.cs
+++ EventGate.Data/Migrations/20240702105909_UpdatePhone.Designer.cs
@@ -4,6 +4,7 @@ using EventGate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventGate.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240702105909_UpdatePhone")]
+    partial class UpdatePhone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
